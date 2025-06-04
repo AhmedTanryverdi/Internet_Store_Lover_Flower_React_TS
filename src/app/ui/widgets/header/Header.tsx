391e7bbe3bbb3653ad71ac7c54b3093124ccd870
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-04 10:05:40 +0300

    header

--- src/app/ui/widgets/header/Header.tsx
+++ src/app/ui/widgets/header/Header.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navigate } from "./ui/navigte/Navigate";
 import { SearchBlock } from "./ui/search-block/SearchBlock";
-import { Logo } from "./ui/logo/Logo";
+import { Logo } from "@/shared/components/logo/Logo";
 import { useLocation } from "react-router-dom";
-import "./styles.scss";
 import { Email } from "./ui/email/Email";
+import "./styles.scss";
+
 
 export const Header: React.FC = (): React.JSX.Element => {
 	const location = useLocation();
