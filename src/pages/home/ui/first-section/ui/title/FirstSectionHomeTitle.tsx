cab32f7b9a73ef5874dfbdbff5f05d1e346b26d7
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-09 00:46:20 +0300

    split home component

---
+++ src/pages/home/ui/first-section/ui/title/FirstSectionHomeTitle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.scss";
+
+export const FirstSectionHomeTitle: React.FC = (): React.JSX.Element => {
+	return (
+		<div className="first-section-home-title">
+			<div className="title">
+				<p>LOVER</p>
+				<p>flower</p>
+			</div>
+			<div className="subtitle">
+				<p>Создаём для тех, кто ценит свежесть и изящество цветка</p>
+			</div>
+		</div>
+	);
+};
