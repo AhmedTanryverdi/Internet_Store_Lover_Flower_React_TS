0b25d324c8ad9d6cf0e79682bf3a98c6386ccba5
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-09 00:43:31 +0300

    fix

--- src/pages/home/ui/first-section/FirstSection.tsx
+++ src/pages/home/ui/first-section/FirstSection.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Button } from "@/shared/components/button/Button";
 import { Background } from "./ui/background/Background";
-import { RightBlock } from "./ui/right-block/RightBlock";
+import { RightContactBlock } from "../../../../shared/components/right-contact-block/RightContactBlock";
+import { FirstSectionHomeTitle } from "./ui/title/FirstSectionHomeTitle";
 import "./styles.scss";
 
 export const FirstSection: React.FC = (): React.JSX.Element => {
 	return (
 		<div className="first-section">
-			<Background />
-			<div className="title">
-				<p>LOVER</p>
-				<p>flower</p>
+			<div className="container">
+				<RightContactBlock />
+				<div className="content">
+					<Background />
+					<FirstSectionHomeTitle />
+					<Button
+						children="смотреть каталог"
+						btnName="home__viewCatalog"
+					/>
+				</div>
 			</div>
-			<div className="subtitle">
-				<p>Создаём для тех, кто ценит свежесть и изящество цветка</p>
-			</div>
-			<Button children="смотреть каталог" btnName="home__viewCatalog" />
-			<RightBlock />
 		</div>
 	);
 };
--- src/pages/home/ui/first-section/ui/background/Background.tsx
+++ src/pages/home/ui/first-section/ui/background/Background.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import bgImage from "@/shared/assets/image/bg.png";
+import bgImage from "@/shared/assets/image/bgImage.png";
 import "./styles.scss";
 
 export const Background: React.FC = (): React.JSX.Element => {
 	return (
 		<div className="home__background">
-			<img src={bgImage} alt="background image" className="background" />
+			<figure className="img-container">
+				<img
+					src={bgImage}
+					alt="background image"
+					className="background"
+				/>
+			</figure>
+			<div className="left-gradient"></div>
+			<div className="right-gradient"></div>
 		</div>
 	);
 };
