3b75f2b96607a56f38d31f34450a7a7b7f451338
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-10 15:41:05 +0300

    added catalog body component

--- src/pages/catalog/Catalog.tsx
+++ src/pages/catalog/Catalog.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CatalogHeader } from "./ui/catalog-header/CatalogHeader";
+import { CatalogBody } from "./ui/catalog-body/CatalogBody";
 import "./styles.scss";
 
 export const Catalog: React.FC = (): React.JSX.Element => {
@@ -8,6 +9,7 @@ export const Catalog: React.FC = (): React.JSX.Element => {
 			<div className="container">
 				<div className="content">
 					<CatalogHeader />
+					<CatalogBody />
 				</div>
 			</div>
 		</div>
