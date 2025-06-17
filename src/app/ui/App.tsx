481e58a80cb575f6073c96a5936ad0180a69291e
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-18 02:46:44 +0300

    added Provider component from react-redux

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./widgets/footer/Footer";
 import { HeaderContainer } from "./widgets/header/HeaderContainer";
+import { Provider } from "react-redux";
+import { store } from "../store";
+
 
 const App: React.FC = (): React.JSX.Element => {
 	return (
-		<>
+		<Provider store={store}>
 			<HeaderContainer />
 			<Outlet />
 			<Footer />
-		</>
+		</Provider>
 	);
 };
 
