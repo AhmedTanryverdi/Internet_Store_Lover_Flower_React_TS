a1ada5e6d9dea1a658926282002c856aa51f13bd
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-19 13:03:38 +0300

    added useQuery

--- src/pages/catalog/ui/catalog-body/CatalogBody.tsx
+++ src/pages/catalog/ui/catalog-body/CatalogBody.tsx
@@ -2,11 +2,36 @@ import React from "react";
 import { SidebarProductsFilter } from "./ui/sidebar-filter/SidebarProductsFilter";
 import { CatalogBackground } from "./ui/background/CatalogBackground";
 import { CatalogFilter } from "./ui/catalog-filter/CatalogFilter";
-import { ProductsBlockContainer } from "./ui/products-block/ProductsBlockContainer";
+import { useQuery, type UseQueryResult } from "@tanstack/react-query";
+import { ProductsBlock } from "./ui/products-block/ProductsBlock";
+import { useSelector } from "react-redux";
+import type { CheckboxFilterType } from "@/shared/lib/types";
+import type { RooteState } from "@/app/store";
+import type { ApiSchemas } from "@/shared/api/schema/index";
+import { buildUrlWithFilter } from "./utils/buildUrl";
 import "./styles.scss";
+
+type FlowerType = ApiSchemas["flower"];
 
 
 export const CatalogBody: React.FC = (): React.JSX.Element => {
+	const filterSetting = useSelector<RooteState, CheckboxFilterType[]>(
+		(state) => state.sideBarFilter.checkboxFilters
+	);
+	const { isPending, error, data }: UseQueryResult<FlowerType[]> = useQuery({
+		queryKey: ["products", filterSetting],
+		queryFn: async () => {
+			const URL = decodeURIComponent(buildUrlWithFilter(filterSetting));
+			console.log("http://localhost:3000/" + URL);
+			const response = await fetch(`http://localhost:3000${URL}`);
+			return await response.json() as FlowerType[];
+		},
+	});
+	if (isPending) {
+		return <h1>loading..</h1>;
+	}
+	if (error) {
+	}
 	return (
 		<div className="catalog-body">
 			<div className="container">
@@ -14,7 +39,7 @@ export const CatalogBody: React.FC = (): React.JSX.Element => {
 				<CatalogFilter />
 				<div className="catalog-body__content">
 					<SidebarProductsFilter />
-					<ProductsBlockContainer />
+					<ProductsBlock flowers={data ?? []} />
 				</div>
 			</div>
 		</div>
