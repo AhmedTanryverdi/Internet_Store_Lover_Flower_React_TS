6af0c7188729259ac9cb556731a0720a202f6125
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-20 03:36:52 +0300

    the logic moved to custom hook

--- src/pages/catalog/ui/catalog-body/CatalogBody.tsx
+++ src/pages/catalog/ui/catalog-body/CatalogBody.tsx
@@ -2,31 +2,13 @@ import React from "react";
 import { SidebarProductsFilter } from "./ui/sidebar-filter/SidebarProductsFilter";
 import { CatalogBackground } from "./ui/background/CatalogBackground";
 import { CatalogFilter } from "./ui/catalog-filter/CatalogFilter";
-import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 import { ProductsBlock } from "./ui/products-block/ProductsBlock";
-import { useSelector } from "react-redux";
-import type { CheckboxFilterType } from "@/shared/lib/types";
-import type { RooteState } from "@/app/store";
-import type { ApiSchemas } from "@/shared/api/schema/index";
-import { buildUrlWithFilter } from "./utils/buildUrl";
+import { useFetchFlowers } from "./utils/customHooks";
 import "./styles.scss";
 
-type FlowerType = ApiSchemas["flower"];
-
-
 export const CatalogBody: React.FC = (): React.JSX.Element => {
-	const filterSetting = useSelector<RooteState, CheckboxFilterType[]>(
-		(state) => state.sideBarFilter.checkboxFilters
-	);
-	const { isPending, error, data }: UseQueryResult<FlowerType[]> = useQuery({
-		queryKey: ["products", filterSetting],
-		queryFn: async () => {
-			const URL = decodeURIComponent(buildUrlWithFilter(filterSetting));
-			console.log("http://localhost:3000/" + URL);
-			const response = await fetch(`http://localhost:3000${URL}`);
-			return await response.json() as FlowerType[];
-		},
-	});
+	const { isPending, error, data } = useFetchFlowers();
+	
 	if (isPending) {
 		return <h1>loading..</h1>;
 	}
