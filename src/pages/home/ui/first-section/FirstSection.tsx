8c662755897b0cdcbad7ffee9ce80b773e9a95ca
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-06-25 00:17:16 +0300

    added navigate

--- src/pages/home/ui/first-section/FirstSection.tsx
+++ src/pages/home/ui/first-section/FirstSection.tsx
@@ -3,9 +3,11 @@ import { Button } from "@/shared/components/button/Button";
 import { Background } from "./ui/background/Background";
 import { RightContactBlock } from "../../../../shared/components/right-contact-block/RightContactBlock";
 import { FirstSectionHomeTitle } from "./ui/title/FirstSectionHomeTitle";
+import { useNavigate } from "react-router-dom";
 import "./styles.scss";
 
 export const FirstSection: React.FC = (): React.JSX.Element => {
+	const naigate = useNavigate();
 	return (
 		<div className="first-section">
 			<div className="container">
@@ -16,6 +18,7 @@ export const FirstSection: React.FC = (): React.JSX.Element => {
 					<Button
 						children="смотреть каталог"
 						btnName="home__viewCatalog"
+						onClick={() => {naigate("/catalog")}}
 					/>
 				</div>
 			</div>
