46d0c86392556364f2596b45e04bbc95ee16f125
AhmedTanryverdi <ahmedtanryverdi@yandex.ru>
2025-07-01 18:43:03 +0300

    added a click on the link

--- src/shared/components/product-card/ProductCard.tsx
+++ src/shared/components/product-card/ProductCard.tsx
@@ -2,15 +2,21 @@ import React, { memo } from "react";
 import { CardHeader } from "./ui/card-header/CardHeader";
 import { CardBody } from "./ui/card-body/CardBody";
 import { CardFooter } from "./ui/card-footer/CardFooter";
-import "./styles.scss";
 import type { ApiSchemas } from "@/shared/api/schema";
+import { useNavigate } from "react-router-dom";
+import {ROUTES} from "@/shared/model/routes"
+import "./styles.scss";
 
 type FlowerType = ApiSchemas["flower"];
 
-export const ProductCard: React.FC<{flower: FlowerType}> = memo(
+export const ProductCard: React.FC<{ flower: FlowerType }> = memo(
 	({ flower }): React.JSX.Element => {
+		const navigate = useNavigate();
 		return (
-			<div className="product-card">
+			<div
+				className="product-card"
+				onClick={() => navigate(`/${ROUTES.CATALOG}/${flower.id}`)}
+			>
 				<CardHeader path={flower.image_url} />
 				<CardBody name={flower.name} price={flower.price} />
 				<CardFooter />
